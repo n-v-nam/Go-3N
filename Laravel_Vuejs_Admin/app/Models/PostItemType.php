7c1243b02f740941196ac72c91598b33b2b78dd0
Do Nam <18020923@vnu.edu.vn>
2022-03-28 22:39:37 +0700

    fix bug

--- Laravel_Vuejs_Admin/app/Models/PostItemType.php
+++ Laravel_Vuejs_Admin/app/Models/PostItemType.php
@@ -4,11 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class PostItemType extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory;
     protected $table = 'post_item_type';
     protected $primaryKey = 'post_item_type_id';
     protected $fillable = [
