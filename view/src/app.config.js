dec9cb6071bab97295db1d501583a70c968e08b8
namnv <18020942@vnu.edu.vn>
2022-04-26 17:14:01 +0700

    Rename main folder backend

--- view/src/app.config.js
+++ view/src/app.config.js
@@ -1,5 +1,5 @@
 export default {
-  LOCAL_API_URL: 'http://localhost:8001',
+  LOCAL_API_URL: 'http://localhost:8000',
   VUE_APP_MAP_ACCESS_TOKEN: 'pk.eyJ1IjoibmFtcm9yZWFsIiwiYSI6ImNsMXZrdnBzYjA3dGkzcXFjNjNpNGM5YnoifQ.XqmjGUbTn5WSexTY3J3nhQ',
   API_MAPBOX_CYCLING: 'https://api.mapbox.com/directions/v5/mapbox/cycling/'
 }
