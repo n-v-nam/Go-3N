c653f82c08d87e0349d3d0be78ad86527408235b
namnv <18020942@vnu.edu.vn>
2022-05-07 01:38:24 +0700

    :+1: update set-up docker

--- view/vue.config.js
+++ view/vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   devServer: {
     disableHostCheck: true,
     host: '0.0.0.0',
-    port: 8088
+    port: 8080
   },
   publicPath: process.env.NODE_ENV === 'production' ? '/Go3.N/' : '/'
 }
